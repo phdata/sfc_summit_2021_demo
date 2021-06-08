c1f7068b8559d9ee81214817bdf1296de0c90669
Nick Hauschild <nhauschild@phdata.io>
2021-06-08 08:58:00 -0500

    Update comments on SQL

--- query/tf_agg_procedure.sql
+++ query/tf_agg_procedure.sql
@@ -1,7 +1,7 @@
 with
     proc_details_tabular as (                                       -- ProcedureDetails 'views' from ui_views
         select
-            filename,
+            filename,                                               -- filename and array_idx make up a unique key for each ProcedureDetails which represents a Procedure
             array_idx,
             to_timestamp(view_obj:EndTime::string, 'YYYY-MON-DD HH24:MI:SS') as proc_end,
             to_timestamp(view_obj:StartTime::string, 'YYYY-MON-DD HH24:MI:SS') as proc_start
@@ -10,7 +10,7 @@ with
         where
             view_name = 'ProcedureDetails'
     ),
-    views_with_proc_idx as (                                        -- ui_views joined with ProcedureDetails using time ranges
+    views_with_proc_idx as (                                        -- ui_views joined with ProcedureDetails by time ranges
         select
             ui_views.filename,
             ui_views.view_name,
@@ -24,17 +24,17 @@ with
             view_name != 'ProcedureDetails'
     )
 select
-    proc_details_tabular.filename,
+    proc_details_tabular.filename,                                  -- proecdure details: key (filename, array_idx), start time, end time, ui views
     proc_idx,
     proc_start,
     proc_end,
-    array_agg(view_name)
+    array_agg(view_name) as views                                   -- array_agg view names to create variant array of all UI views in a given Procedure
 from
     proc_details_tabular
     join views_with_proc_idx on (proc_details_tabular.filename = views_with_proc_idx.filename
                                     and proc_details_tabular.array_idx = views_with_proc_idx.proc_idx)
 group by
-    proc_details_tabular.filename,
+    proc_details_tabular.filename,                                  -- group by Procedure key (filename, array_idx) to have a row for each Procedure
     proc_idx,
     proc_start,
     proc_end;
--- query/tf_prepare_actions.sql
+++ query/tf_prepare_actions.sql
@@ -1,5 +1,5 @@
 select
-    view_field.seq as                       prepare_view_key,       -- 3) a key to uniquely identify each Prepare view
+    view_field.seq as                       prepare_view_key,       -- 3) a key to the Prepare View this field is from
     iff(
         is_object(view_field.value),
         'attempt',
@@ -10,15 +10,15 @@ select
         'YYYY-MON-DD HH24:MI:SS'
     ) as                                    prepare_action_time,    -- 5) action timestamp, parsed from field key
     view_field.value:attempt::number as     attempt_num,            -- 6) action details, parsed from field value when it is an object, null if not an object.
-    view_field.value:error::number as       error,
-    view_field.value:xDim::number as        x,
-    view_field.value:yDim::number as        y,
-    view_field.value:zDim::number as        z
+    view_field.value:error::number as       error,                  --    same ^^
+    view_field.value:xDim::number as        x,                      --    same   ^^
+    view_field.value:yDim::number as        y,                      --    same     ^^
+    view_field.value:zDim::number as        z                       --    same       ^^
 from
     ui_views,
-    lateral flatten(input => view_obj) view_field                   -- 1) explode all 'View' objects (from views_tabular CTE)
+    lateral flatten(input => view_obj) view_field                   -- 1) explode all 'View' objects (from ui_views), creating a row for each field of the views
 where
-    view_name = 'Prepare'                                           -- 2) filter all View fields to those from Prepare views with keys that parse to a timestamp of the given format
+    view_name = 'Prepare'                                           -- 2) filter all rows to fields from Prepare views with keys that parse to a timestamp of the given format
     and try_to_timestamp(
         view_field.key, 
         'YYYY-MON-DD HH24:MI:SS'
